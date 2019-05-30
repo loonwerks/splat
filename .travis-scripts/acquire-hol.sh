eb551b8e1ff6ffa424be6191cab9ae21b7c9a0f1
Karl Hoech <karl.hoech@collins.com>
2019-05-30 09:39:41 -0500

    Don't use maven/ant to build

---
+++ .travis-scripts/acquire-hol.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+HOL_BASE="https://github.com/HOL-Theorem-Prover/HOL.git"
+
+
+# Defaults:
+HOL_CHECKOUT=${HOL_CHECKOUT:develop}
+SML=${SML:poly}
+
+git clone $HOL_BASE
+pushd HOL
+git checkout $HOL_CHECKOUT
+eval `opam config env` && $SML < tools/smart-configure.sml && bin/build $BUILDOPTS --nograph
+popd
