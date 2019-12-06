fb43ce77ea42341966b6ef5eee93562642d7d1c4
kfhoech <kfhoech@users.noreply.github.com>
2019-12-06 12:56:05 -0600

    Update acquire-hol.sh
    Change HOL clone command to explicitly name branch.

--- .travis-scripts/acquire-hol.sh
+++ .travis-scripts/acquire-hol.sh
@@ -9,8 +9,7 @@ HOL_BASE="https://github.com/HOL-Theorem-Prover/HOL.git"
 HOL_CHECKOUT=${HOL_CHECKOUT:master}
 SML=${SML:poly}
 
-git clone $HOL_BASE
+git clone -b $HOL_CHECKOUT $HOL_BASE
 pushd HOL
-git checkout $HOL_CHECKOUT
 eval `opam config env` && $SML < tools/smart-configure.sml && bin/build $BUILDOPTS --nograph
 popd
