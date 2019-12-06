9a3e87e522edfe139a730c6f60ebb1e7d6cc2168
kfhoech <kfhoech@users.noreply.github.com>
2019-12-06 11:32:37 -0600

    Update acquire-hol.sh
    Everything SPLAT needs merged to HOL master; no longer need bleeding edge of HOL development.

--- .travis-scripts/acquire-hol.sh
+++ .travis-scripts/acquire-hol.sh
@@ -6,7 +6,7 @@ HOL_BASE="https://github.com/HOL-Theorem-Prover/HOL.git"
 
 
 # Defaults:
-HOL_CHECKOUT=${HOL_CHECKOUT:develop}
+HOL_CHECKOUT=${HOL_CHECKOUT:master}
 SML=${SML:poly}
 
 git clone $HOL_BASE
