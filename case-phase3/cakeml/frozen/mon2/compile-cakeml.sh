2efd17643cca741cfdc3024a2f4ab1000b610a3c
Karl Hoech <karl.hoech@collins.com>
2021-06-07 18:01:50 -0500

    change build script to optimized mon

--- case-phase3/cakeml/frozen/mon2/compile-cakeml.sh
+++ case-phase3/cakeml/frozen/mon2/compile-cakeml.sh
@@ -4,7 +4,7 @@ set -e
 
 export SCRIPT_HOME=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
 cd $SCRIPT_HOME
-export BASE_NAME=mon2.hamr
+export BASE_NAME=mon2.opt.hamr
 ~/bin/cake-x64-32/cake --target=arm7 --heap_size=4 --stack_size=4 < ${SCRIPT_HOME}/${BASE_NAME}.cml > ${SCRIPT_HOME}/${BASE_NAME}.S
 sed -i 's/cdecl(main)/cdecl(run)/' ${SCRIPT_HOME}/${BASE_NAME}.S
 
